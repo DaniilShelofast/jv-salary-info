1eed9ec7a2ce23f78602e4d972719341c6f9b290
Danyil <shelofast.daniil@gmail.com>
2025-06-23 19:18:23 +0300

    homework

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,55 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+
+    private static final int INDEX_DATE = 0;
+    private static final int INDEX_NAME = 1;
+    private static final int INDEX_TIME = 2;
+    private static final int INDEX_MONEY = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        if (names.length == 0 || data.length == 0 || dateFrom == null || dateTo == null) {
+            return null;
+        }
+
+        if (dateFrom.isBlank() || dateTo.isBlank()) {
+            return null;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder builder = new StringBuilder();
+        int[] moneyUserAndName = new int[names.length];
+        for (String line : data) {
+            String[] userData = line.split(" ");
+            for (int i = 0; i < names.length; i++) {
+                LocalDate timeEmployee = LocalDate.parse(userData[INDEX_DATE], formatter);
+                if ((timeEmployee.isEqual(from) || timeEmployee.isAfter(from))
+                        && (timeEmployee.isEqual(to) || timeEmployee.isBefore(to))) {
+                    if (names[i].equals(userData[INDEX_NAME])) {
+                        moneyUserAndName[i] += (Integer.parseInt(userData[INDEX_TIME])
+                                * Integer.parseInt(userData[INDEX_MONEY]));
+                    }
+                }
+            }
+        }
+
+        builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
+
+        for (int i = 0; i < moneyUserAndName.length; i++) {
+            builder.append(System.lineSeparator())
+                    .append(names[i])
+                    .append(" - ")
+                    .append(moneyUserAndName[i]);
+        }
+
+        return builder.toString();
     }
 }
+
